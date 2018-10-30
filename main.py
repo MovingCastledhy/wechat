55e176d1722ff43f27c6a3a5ae3e9b2dd65506a1
donghaiyan <donghaiyan@nuanxin-health.com>
2018-10-30 11:32:10 +0800

    模板消息

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 # -*- coding:utf-8 -*-
 
 from flask import Flask, request, make_response
+from flask import current_app, json, abort, redirect, url_for, session, g, Response
 import hashlib
 import xmltodict
 import time
+from template import *
 
 app = Flask(__name__)
 
+APP_ID = 'wx52aca0ee0b6a6769'
+APP_SECRET = '3336b036596e817abc9a965bb78fd71a'
 WECHAT_TOKEN = "donghaiyan"
 
 
@@ -34,52 +38,141 @@ def wechat():
         if request.method == "GET":
             return echostr
         else:
-            """收发消息接口"""
-            req_xml = request.data
-            req = xmltodict.parse(req_xml)['xml']
-            if "text" == req.get("MsgType"):
-                resp = {
-                    "ToUserName": req.get("FromUserName", ""),
-                    "FromUserName": req.get("ToUserName", ""),
-                    "CreateTime": int(time.time()),
-                    "MsgType": "text",
-                    "Content": req.get("Content", "")
-                }
-            else:
-                resp = {
-                    "ToUserName": req.get("FromUserName", ""),
-                    "FromUserName": req.get("ToUserName", ""),
-                    "CreateTime": int(time.time()),
-                    "MsgType": "text",
-                    "Content": "费文本"
-                }
-            resp_xml = xmltodict.unparse({"xml": resp})
-            return resp_xml
 
-            # resp_data = request.data
-            # resp_dict = xmltodict.parse(resp_data).get('xml')
-            #
-            # # 如果是文本消息
-            # if 'text' == resp_dict.get('MsgType'):
-            #     response = {
-            #         "ToUserName": resp_dict.get('FromUserName'),
-            #         "FromUserName": resp_dict.get('ToUserName'),
+            # """收发消息接口，收到文本原封不动返回"""
+            # req_xml = request.data
+            # req = xmltodict.parse(req_xml)['xml']
+            # if "text" == req.get("MsgType"):
+            #     resp = {
+            #         "ToUserName": req.get("FromUserName", ""),
+            #         "FromUserName": req.get("ToUserName", ""),
             #         "CreateTime": int(time.time()),
             #         "MsgType": "text",
-            #         "Content": resp_dict.get('Content'),
+            #         "Content": req.get("Content", "")
             #     }
-            #     print(resp_dict.get('Content'))
-
             # else:
-            #     response = {
-            #     "ToUserName": resp_dict.get('FromUserName'),
-            #     "FromUserName": resp_dict.get('ToUserName'),
-            #     "CreateTime": int(time.time()),
-            #     "MsgType": "text",
-            #     "Content": u"哈哈哈哈",
-            # }
-            #
-            # return make_response(response)
+            #     resp = {
+            #         "ToUserName": req.get("FromUserName", ""),
+            #         "FromUserName": req.get("ToUserName", ""),
+            #         "CreateTime": int(time.time()),
+            #         "MsgType": "text",
+            #         "Content": "费文本"
+            #     }
+            # resp_xml = xmltodict.unparse({"xml": resp})
+            # return resp_xml
+
+        """模板消息"""
+        access_token = get_token(APP_ID,APP_SECRET)
+        current_app.logger.info(access_token)
+        sent_url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=" + access_token
+        message = {
+            "touser": 'oW4bJ1fvxaeGg2Bqhd4-gzaS4EJE',
+            "template_id": "ngqIpbwh8bUfcSsECmogfXcV14J0tQlEpBO27izEYtY",
+            "url": "http://weixin.qq.com/download",
+            "topcolor": "#FF0000",
+
+            "data": {
+
+                "User": {
+
+                    "value": "黄先生",
+
+                    "color": "#173177"
+
+                },
+
+                "Date": {
+
+                    "value": "06月07日 19时24分",
+
+                    "color": "#173177"
+
+                },
+
+                "CardNumber": {
+
+                    "value": "0426",
+
+                    "color": "#173177"
+
+                },
+
+                "Type": {
+
+                    "value": "消费",
+
+                    "color": "#173177"
+
+                },
+
+                "Money": {
+
+                    "value": "人民币260.00元",
+
+                    "color": "#173177"
+
+                },
+
+                "DeadTime": {
+
+                    "value": "06月07日19时24分",
+
+                    "color": "#173177"
+
+                },
+
+                "Left": {
+
+                    "value": "6504.09",
+
+                    "color": "#173177"
+
+                }
+
+            }
+        }
+
+        # 推送消息
+        # if not access_token is None:
+            # conn = httplib.HTTPConnection("api.weixin.qq.com:80")  # 微信接口链接
+            # headers = {"Content-type": "application/json"}  # application/x-www-form-urlencoded
+            # params = ({'touser': "oEGZ4johnKOtayJbnEVeuaZr6zQ0",  # 用户openid
+            #            'template_id': 'AtFuydv8k_15UGZuFntaBzJRCsHCkjNm1dcWD3A-11Y',  # 模板消息ID
+            #            'url': 'http://www.710.so',  # 跳转链接
+            #            "topcolor": "#667F00",  # 颜色
+            #            "data": {  # 模板内容
+            #                "first": {"value": "尊敬的710.so : 您的网站http://www.710.so (192.168.1.1) 有异常访问",
+            #                          "color": "#173177"},
+            #                "keyword1": {"value": "访问时间 2015-04-05 15:30:59 访问IP 192.168.1.2", "color": "#173177"},
+            #                "keyword2": {"value": "访问链接 http://www.710.so", "color": "#173177"},
+            #                "remark": {"value": "访问频率 10/s", "color": "#173177"}
+            #            }
+            #            }
+            #           )
+            # conn.request("POST", "/cgi-bin/message/template/send?access_token=" + access_token,
+            #              json.JSONEncoder().encode(params), headers)  # 推送消息请求
+            # response = conn.getresponse()
+            # data = response.read()  # 推送返回数据
+            # if response.status == 200:
+            #     print
+            #     'success'
+            #     print
+            #     data
+            # else:
+            #     print
+            #     'fail'
+            # conn.close()
+
+            headers = {
+                'Content-Type': 'application/json',
+                'encoding': 'utf-8',
+            }
+            response = requests.post(sent_url, json.dumps(message), headers=headers)
+            print(response)
+
+
+
+
     else:
         return 'errno', 403
 
---
+++ template.py
@@ -0,0 +1,147 @@
+#! /usr/bin/env python
+# coding: utf-8
+
+import sys
+import requests
+import json
+
+from datastore.mongo import dm
+
+
+def get_token(app_id, app_secret):
+    url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={}&secret={}'.format(app_id, app_secret)
+    result = requests.get(url)
+    access_token = json.loads(result.content.decode()).get('access_token')
+    print(access_token)
+    return access_token
+
+
+def get_formatter_list(formatters, keys):
+    try:
+        return [formatters[key] for key in keys]
+    except (ValueError, KeyError):
+        return []
+
+
+def send_template(platform_name, openids, template_name, url, formatters):
+    # send_template('nxzl0', openids, 'out_hospital', url, {'out_date': '2018年9月5日', 'patient_name': '王小瑶'})
+    wechat = dm.Wechat.objects(name=platform_name).first()
+    if wechat is None:
+        return None
+    access_token = get_token(wechat.appid, wechat.appsecret)
+    sent_url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=" + access_token
+    template_map = {
+        'service_start': {
+            'template_id': 'L3OxnRB-kFZwmwbNhLxDP3IzMpYsHzB5yd7N6KueKnA',
+            'url': url,
+            'data': {
+                'first': {
+                    'template': '你签约的全病程管理服务已开通，请您下载App并登录开始享受服务。', 'formatter': [],
+                },
+                'keyword1': {
+                    'template': '{}', 'formatter': ['start_date'],
+                },
+                'keyword2': {
+                    'template': '全病程管理服务', 'formatter': [],
+                },
+                'keyword3': {
+                    'template': '您将获得在线问诊、在线购买药品、医生随访关怀、服药提醒、专家宣教等服务。帮助您提高治疗效率，早日康复。', 'formatter': [],
+                },
+                'keyword4': {
+                    'template': '', 'formatter': [],
+                },
+                'remark': {
+                    'template': '点击详情查看全病程管理服务。', 'formatter': [],
+                },
+            }
+        },
+        'out_hospital': {
+            'template_id': 'LdMU0AHVcAqw8dkMtgXyJGi2yohwNjirfe5lAuO91dI',
+            'url': url,
+            'data': {
+                'first': {
+                    'template': '为了您能在出院后更快的康复，我们为您开启了院后的全病程管理服务。', 'formatter': [],
+                },
+                'keyword1': {
+                    'template': '{}', 'formatter': ['patient_name'],
+                },
+                'keyword2': {
+                    'template': '{}', 'formatter': ['out_date'],
+                },
+                'keyword3': {
+                    'template': '', 'formatter': [],
+                },
+                'keyword4': {
+                    'template': '', 'formatter': [],
+                },
+                'remark': {
+                    'template': '点击详情查看全病程管理服务。', 'formatter': [],
+                },
+            }
+        }
+    }
+
+    if template_name not in template_map:
+        return
+    template = template_map[template_name]
+    for o in openids:
+        message = {
+            "touser": o,
+            "template_id": template['template_id'],
+            "url": template['url'],
+            "data": {
+                "first": {
+                    "value": template['data']['first']['template'].format(
+                        *get_formatter_list(formatters, template['data']['first']['formatter'])),
+                    "color": template['data']['first'].get('color') or '#173177'
+                },
+                "keyword1": {
+                    "value": template['data']['keyword1']['template'].format(
+                        *get_formatter_list(formatters, template['data']['keyword1']['formatter'])),
+                    "color": template['data']['keyword1'].get('color') or '#173177'
+                },
+                "keyword2": {
+                    "value": template['data']['keyword2']['template'].format(
+                        *get_formatter_list(formatters, template['data']['keyword2']['formatter'])),
+                    "color": template['data']['keyword2'].get('color') or '#173177'
+                },
+                "keyword3": {
+                    "value": template['data']['keyword3']['template'].format(
+                        *get_formatter_list(formatters, template['data']['keyword3']['formatter'])),
+                    "color": template['data']['keyword3'].get('color') or '#173177'
+                },
+                "keyword4": {
+                    "value": template['data']['keyword4']['template'].format(
+                        *get_formatter_list(formatters, template['data']['keyword4']['formatter'])),
+                    "color": template['data']['keyword4'].get('color') or '#173177'
+                },
+                "remark": {
+                    "value": template['data']['remark']['template'].format(
+                        *get_formatter_list(formatters, template['data']['remark']['formatter'])),
+                    "color": template['data']['remark'].get('color') or '#173177'
+                }
+            }
+        }
+
+        headers = {
+            'Content-Type': 'application/json',
+            'encoding': 'utf-8',
+        }
+        response = requests.post(sent_url, json.dumps(message), headers=headers)
+        print(response)
+
+
+if __name__ == '__main__':
+    openids = [
+        'orTkYw__M9vrJ1YIDZW4VGJmoqLs',
+        # 'orTkYw-Ba6lXwHWuIHwom-PgAqPI',
+        # 'orTkYw2hNk_VioBaAa3LizoGu78I',
+        # 'orTkYw47Shlf9vb-mHp7lMCIBqyY',
+        # 'orTkYw-2_0Iv3pl02-q7MNN4pblk',
+        # 'orTkYwx-g0SIhALcW-Pbxm0w_qwk',
+    ]
+    url = 'https://www.baidu.com'
+    # send_template('nxzl0', openids, 'service_start', url, {'start_date': '2018年9月5日'})
+    send_template('nxzl0', openids, 'out_hospital', url, {'out_date': '2018年9月5日', 'patient_name': '王小瑶'})
+
+
